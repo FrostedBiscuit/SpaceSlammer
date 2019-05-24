d44875444899862e53cb70662592304b4e704c45
Fr0$t <kaucic.gasper@gmail.com>
2019-05-24 15:58:21 +0200

    Added different prefab variants of the bacis PlayerShip, the player can also switch between them and the index is saved

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -5,17 +5,57 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour {
 
     [SerializeField]
-    GameObject PlayerGO;
+    List<GameObject> PlayerGOs = new List<GameObject>();
 
     [SerializeField]
     Transform Spawn;
 
     [SerializeField]
     CameraFollow cameraFollow;
+#if UNITY_EDITOR
+    [SerializeField]
+    int DEBUG_TestPlayerGOIndex = 0;
+#endif
+    int currentIndex {
+        get {
+            return _currIndex;
+        }
+        set {
+            _currIndex = value;
+
+            SpawnPlayer();
+        }
+    }
+
+    int _currIndex = 0;
 
     // Start is called before the first frame update
     void Start() {
-        
+#if UNITY_EDITOR
+        if (PlayerPrefs.HasKey("CurrentPlayerGOIndex") == true && PlayerPrefs.GetInt("CurrentPlayerGOIndex") == DEBUG_TestPlayerGOIndex) {
+            currentIndex = PlayerPrefs.GetInt("CurrentPlayerGOIndex");
+        }
+        else {
+            PlayerPrefs.SetInt("CurrentPlayerGOIndex", DEBUG_TestPlayerGOIndex);
+            currentIndex = DEBUG_TestPlayerGOIndex;
+        }
+
+        if (Player.instance == null || PlayerGOs.IndexOf(Player.instance.gameObject) != DEBUG_TestPlayerGOIndex) {
+
+            SpawnPlayer();
+        }
+#else
+        if (PlayerPrefs.HasKey("CurrentPlayerGOIndex") == true) {
+            currentIndex = PlayerPrefs.GetInt("CurrentPlayerGOIndex");
+        }
+        else {
+            PlayerPrefs.SetInt("CurrentPlayerGOIndex", 0);
+        }
+
+        if (Player.instance == null) {
+            SpawnPlayer();
+        }
+#endif
     }
 
     // Update is called once per frame
@@ -32,11 +72,24 @@ public class PlayerManager : MonoBehaviour {
 
     public void SpawnPlayer() {
 
-        if (Player.instance == null) {
-
-            GameObject go = ObjectPool.instance.RequestObject(PlayerGO, Spawn.position, Spawn.rotation);
-
-            cameraFollow.FollowTransform = go.transform;
+        if (Player.instance != null) {
+            
+            ObjectPool.instance.ReturnObject(Player.instance.gameObject);
         }
+
+        GameObject go = ObjectPool.instance.RequestObject(PlayerGOs[currentIndex], Spawn.position, Spawn.rotation);
+
+        cameraFollow.FollowTransform = go.transform;
+
+        PlayerPrefs.SetInt("CurrentPlayerGOIndex", currentIndex);
+    }
+
+    public void IncreasePlayerGOIndex() {
+        currentIndex = (currentIndex + 1) % PlayerGOs.Count;
+    }
+
+    public void DecreasePlayerGOIndex() {
+
+        currentIndex = (currentIndex - 1) < 0 ? PlayerGOs.Count - 1 : currentIndex - 1;
     }
 }
