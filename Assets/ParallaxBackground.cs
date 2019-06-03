d476be7df5002bb22340abdb052bb97cdd8fb66e
Fr0$t <kaucic.gasper@gmail.com>
2019-06-03 15:07:15 +0200

    Added parallax background

---
+++ Assets/ParallaxBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour {
+
+    public float ParallaxCoeficient = 3f;
+
+    void Update() {
+
+        Material mat = GetComponent<MeshRenderer>().material;
+
+        Vector2 offset = new Vector2(transform.position.x / transform.localScale.x / ParallaxCoeficient, transform.position.y / transform.localScale.y / ParallaxCoeficient);
+
+        mat.mainTextureOffset = offset;
+    }
+}
